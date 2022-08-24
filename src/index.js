604d6c29a264bf6607b1fd3d197c185ca8c0fb8d
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-08-24 21:38:56 +0100

    updated UI by using stored API data

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import {
     fahrenheitToCelcius,
 } from "./temperatureMeasurements"
 
+import format from "date-fns/format"
+
 export const DOM = {
     clouds: document.querySelector(".clouds"),
     city: document.querySelector(".city"),
     date: document.querySelector(".date"),
     time: document.querySelector(".time"),
     temp: document.querySelector(".main-temp"),
+    weatherImg: document.querySelector(".main-img"),
     displayF: document.querySelector(".change-temp-type-F"),
     displayC: document.querySelector(".change-temp-type-C"),
 }
@@ -21,6 +24,10 @@ DOM.displayC.addEventListener("click", () => {
     return fahrenheitToCelcius(DOM.temp.innerHTML)
 })
 
+let locationData
+
+getLocation("china")
+
 async function getLocation(city) {
     const response = await fetch(
         `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=3019f42598e2771e78de4788949e7fff`,
@@ -29,14 +36,14 @@ async function getLocation(city) {
     response
         .json()
         .then(function (response) {
-            return getTemp(`lat=${response[0].lat}&lon=${response[0].lon}`)
+            return getData(`lat=${response[0].lat}&lon=${response[0].lon}`)
         })
         .catch(function () {
             console.log("failure")
         })
 }
 
-async function getTemp(latAndLon) {
+async function getData(latAndLon) {
     const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?${latAndLon}&appid=3019f42598e2771e78de4788949e7fff`,
         {
@@ -46,12 +53,63 @@ async function getTemp(latAndLon) {
     response
         .json()
         .then(function (response) {
-            console.log(response)
-            DOM.temp.innerHTML = kelvinToCelcius(response.main.temp) + " °C"
+            return (locationData = response), console.log(response), updateUI()
         })
         .catch(function () {
-            console.log("failure in gettemp")
+            console.log("failed to get data")
         })
 }
 
-getLocation("tokyo")
+export function updateUI() {
+    DOM.temp.innerHTML = kelvinToCelcius(locationData.main.temp) + " °C"
+    DOM.clouds.innerHTML =
+        locationData.weather[0].description.charAt(0).toUpperCase() +
+        locationData.weather[0].description.slice(1)
+    DOM.city.innerHTML = locationData.name
+
+    weatherImg(locationData.weather[0].description)
+    function weatherImg(weather) {
+        if (weather.includes("clouds") === true) {
+            DOM.weatherImg.outerHTML = `<img
+            class="main-img"
+            src="./images/weather pngs/clouds.png"
+            alt="clouds"
+        />`
+        }
+        if (weather.includes("rain") === true) {
+            DOM.weatherImg.outerHTML = `<img
+            class="main-img"
+            src="./images/weather pngs/rain.png"
+            alt="rain"
+        />`
+        }
+        if (weather.includes("sun") === true) {
+            DOM.weatherImg.outerHTML = `<img
+            class="main-img"
+            src="./images/weather pngs/sun.png"
+            alt="sun"
+        />`
+        }
+        if (weather.includes("snow") === true) {
+            DOM.weatherImg.outerHTML = `<img
+            class="main-img"
+            src="./images/weather pngs/snow.png"
+            alt="sun"
+        />`
+        }
+        if (weather.includes("partly") === true) {
+            DOM.weatherImg.outerHTML = `<img
+            class="main-img"
+            src="./images/weather pngs/partly cloudy.png"
+            alt="sun"
+        />`
+        }
+        if (weather.includes("thunderstorm") === true) {
+            DOM.weatherImg.outerHTML = `<img
+            class="main-img"
+            src="./images/weather pngs/thunderstorm.png"
+            alt="sun"
+        />`
+        }
+    }
+}
