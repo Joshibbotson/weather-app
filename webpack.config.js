890e716b162999f429dc20dd9c94b623018dc30b
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-01-08 18:02:59 +0000

    CHORE - fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,16 @@ module.exports = {
                     },
                 },
             },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[ext]",
+                        outputPath: "images/",
+                    },
+                },
+            },
         ],
     },
 }
