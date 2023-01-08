130f47e0ec23fe2523e9a3e96e283964e610c3c0
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-01-08 18:01:15 +0000

    CHORE - updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
+const path = require("path")
+
 module.exports = {
     entry: "./src/index.js",
-    mode: "development",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
     module: {
         rules: [
             {
-                test: /\.css/,
-                use: ["style-loader", "css-loader", "sass-loader"],
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
             },
             {
                 test: /\.js$/,
-                exclude: /(node_modules)/,
+                exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
                 },
             },
         ],
